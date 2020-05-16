270234ff4017adab809383aed8bce79c81c90ef3
unknown <projetosmobimark@gmail.com>
2020-05-16 15:04:27 -0300

    :construction: redux user finished

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './assets/css/grid.css'
-import 'animate.css'
+import './assets/css/animate.css'
 
 ReactDOM.render(
   <App />,document.getElementById('root')
